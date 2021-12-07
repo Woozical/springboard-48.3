0d0b1ad322f97c909b4ebe9c770f8ee0b2defaad
Mark <mark@example.dev>
2021-12-07 12:20:12 -0800

    curried-add

--- curried-add.js
+++ curried-add.js
@@ -1,5 +1,8 @@
 function curriedAdd(total) {
-
+  return (total === undefined) ? 0 : function(num){
+    if (num === undefined) return total;
+    return curriedAdd(total + num);
+  }
 }
 
 module.exports = { curriedAdd };
