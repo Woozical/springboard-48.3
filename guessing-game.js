3df156150fddb54de41c14f989a018af31a87156
Mark <mark@example.dev>
2021-12-07 11:58:06 -0800

    guessing-game

--- guessing-game.js
+++ guessing-game.js
@@ -1,5 +1,16 @@
 function guessingGame() {
-
+  const target = Math.floor(Math.random() * 100);
+  let hasWon = false;
+  let guessCount = 0;
+  return function(guess){
+    if (hasWon) return "The game is over, you already won!";
+    guessCount++;
+    if (guess === target){
+      hasWon = true;
+      return `You win! You found ${target} in ${guessCount} guesses.`
+    }
+    return guess > target ? `${guess} is too high!` : `${guess} is too low!`;
+  }
 }
 
 module.exports = { guessingGame };
